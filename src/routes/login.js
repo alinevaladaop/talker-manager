8dcbc8750ddf73b67e5b22709f00f41586616e7b
Aline Valadao <valadao.aline@gmail.com>
2022-11-04 21:06:32 +0100

    Requisito 03

---
+++ src/routes/login.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const router = express.Router();
+
+function randomToken(length) { // function from stackoverflow
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i += 1) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+router.post('/', async (req, res) => 
+  // const { email, password } = req.body;
+     res.status(200).json({
+      token: randomToken(16),
+    }));
+
+module.exports = router;
